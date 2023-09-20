4710b17497e5563957f2c2fb339d269f05e10526
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-20 17:22:50 +0300

    paddings

--- Lab 4- User Interface (UI) development- Components and Layouts/Tutorial/JepackCompose/app/src/main/java/com/example/jepackcompose/MainActivity.kt
+++ Lab 4- User Interface (UI) development- Components and Layouts/Tutorial/JepackCompose/app/src/main/java/com/example/jepackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jepackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,10 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,14 +71,34 @@ fun MyApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 10.dp , horizontal = 10.dp),
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 10.dp, horizontal = 10.dp),
 
         ) {
-        Text(
-            text = "Hello Ms. $name!",
-            modifier = modifier.padding(24.dp)
-        )
+        Row {
+            Text(
+                text = "Hello Ms. $name!",
+                modifier = modifier
+                    .padding(24.dp)
+                    .weight(1f)
+            )
+            ElevatedButton(
+                onClick = {
+                    expanded = !expanded
+                    Log.d("EXPANDED", "$expanded")
+                },
+                modifier = modifier
+                    .padding(24.dp)
+                    .weight(1f)
+            ) {
+                Text(text = if(expanded) "Show Less" else "Show More")
+            }
+        }
     }
 
 }
