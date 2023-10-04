d9cd0a4620b4d65fe760b22250a7c81fdec154ca
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-04 15:51:57 +0300

    updated code

---
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/model/Stadium.kt
@@ -0,0 +1,19 @@
+package com.example.stadiums.model
+
+data class Stadium(
+ val name : String,
+ val city : String,
+ val status : String,
+ val imageName : String,
+)
+/*
+
+  {
+    "name": "Al-Gharafa Stadium",
+    "city": "Al-Rayyan",
+    "status": "Major renovation",
+    "imageName": "al_gharafa",
+    "seatingCapacity": 2412
+  },
+
+ */
