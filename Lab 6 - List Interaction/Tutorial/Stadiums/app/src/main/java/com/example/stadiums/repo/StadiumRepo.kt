1ac4ba4d32a57fbba0d4b91dd70d01463f7187c9
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-04 16:01:53 +0300

    updated code for repo

--- Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/repo/StadiumRepo.kt
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/repo/StadiumRepo.kt
@@ -1,4 +1,28 @@
 package com.example.stadiums.repo
 
+import android.content.Context
+import com.example.stadiums.model.Stadium
+import kotlinx.serialization.json.Json
+
 object StadiumRepo {
+    //    a variable that can strore the stadiums
+    var stadiums = listOf<Stadium>()
+
+    fun getStadiums(context: Context): List<Stadium> {
+        if (stadiums.isEmpty()){
+            //        text content of the file can be read as
+            val stadiumJsonText = context
+                .assets
+                .open("stadiums.json")
+                .bufferedReader()
+                .use {
+                    it.readText()
+                }
+//        convert the json text to a list of stadiums
+            stadiums = Json { ignoreUnknownKeys = true }
+                .decodeFromString(stadiumJsonText)
+        }
+
+        return stadiums
+    }
 }
