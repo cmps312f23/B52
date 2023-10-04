dec4df97ee6182f609f0451f025a1edf0e9ecdd2
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-04 16:21:13 +0300

    updated code for stadium list start code

--- Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/view/StadiumList.kt
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/view/StadiumList.kt
@@ -1,16 +1,28 @@
 package com.example.stadiums.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stadiums.model.Stadium
+import com.example.stadiums.ui.theme.StadiumsTheme
 
 @Composable
 fun StadiumCard(stadium: Stadium, modifier: Modifier = Modifier) {
-
+    Text(text = stadium.name)
 }
+
 @Preview
 @Composable
-fun StadiumCardPreview(stadium: Stadium, modifier: Modifier = Modifier) {
-
+fun StadiumCardPreview() {
+    val stadium = Stadium(
+        name = "Al Wakra",
+        seatingCapacity = 1200,
+        status = "Built",
+        city = "Doha",
+        imageName = "al_wakra"
+    )
+    StadiumsTheme {
+        StadiumCard(stadium)
+    }
 }
